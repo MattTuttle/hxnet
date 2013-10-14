2e7d28c0d1daf8b63e4945115d682aae838ce4d9
Matt Tuttle <matt.tuttle@augusthome.com>
2013-10-14 16:37:11 -0500

    added really simple echo on/off

--- hxnet/protocols/Telnet.hx
+++ hxnet/protocols/Telnet.hx
@@ -2,6 +2,7 @@ package hxnet.protocols;
 
 import haxe.io.Input;
 import haxe.io.Bytes;
+import haxe.io.BytesOutput;
 
 enum Color
 {
@@ -34,6 +35,9 @@ class Telnet extends BaseProtocol
 	public override function dataReceived(input:Input)
 	{
 		var buffer = input.readLine();
+		// filter out IAC commands for now
+		if (buffer.charCodeAt(0) == 0xFF)
+			return;
 
 		if (promptCallback != null)
 		{
@@ -62,8 +66,19 @@ class Telnet extends BaseProtocol
 	public function writeLine(data:String, reset:Bool=false)
 	{
 		if (reset) data += text(Reset); // reset to normal text
-		data += "\n";
+		data += "\r\n";
 		cnx.writeBytes(Bytes.ofString(data));
+	}
+
+	public function echo(show:Bool = true)
+	{
+		var out = new BytesOutput();
+		out.writeByte(0xFF);
+		out.writeByte(show ? 0xFC : 0xFB);
+		out.writeByte(0x01);
+		if (show)
+			out.writeString("\r\n");
+		cnx.writeBytes(out.getBytes());
 	}
 
 	public function prompt(prompt:String, callback:String->Bool)
